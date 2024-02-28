0b16f1d81d8d0a0af0fe946bbe6ba47bb3c80da9
temirbaevabermet <temirbaevabermet4@gmail.com>
2024-02-28 18:51:01 +0600

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+let buttonElement = document.querySelector("button");
+buttonElement.onclick = () =>{
+    alert("Can you enter your name?");
+    
+}
+let btn = "OK";
+const myHeading = document.querySelector("h1");
+function changeHeader(){
+    let name = document.getElementById("name").value;
+    myHeading.textContent = "Welecome"+" "+ name+"!";
+}
+
+const imgElement = document.querySelector("img");
+const img1URL = "images/Ala-Too_International_University_Seal.png";
+const img2URL = "images/nature-photo.webp";
+function changePhoto(event){
+    let current = imgElement.getAttribute("src");
+    if(current == img1URL){
+        imgElement.setAttribute("src", img2URL);
+    }
+    else{
+        imgElement.setAttribute("src",img1URL);
+    }
+}
+imgElement.onclick = changePhoto;
