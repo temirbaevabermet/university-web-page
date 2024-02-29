dabe834107e7169932b73b2eb539cb852c4561c3
temirbaevabermet <150426617+temirbaevabermet@users.noreply.github.com>
2024-02-29 15:28:44 +0700

    Add files via upload

---
+++ web-design/web-design/scripts/main.js
@@ -0,0 +1,26 @@
+let buttonElement = document.querySelector("button");
+buttonElement.onclick = () =>{
+    alert("Can you enter your name?");
+    
+}
+let btn = "OK";
+const myHeading = document.querySelector("h1");
+function changeHeader(){
+    let name = document.getElementById("name").value;
+    myHeading.textContent = "Welecome"+" "+ name+"!";
+}
+
+const imgElement = document.querySelector("img");
+const img1URL = "images/alatoo.png";
+const img2URL = "images/nature-photo.webp";
+function changePhoto(event){
+    let current = imgElement.getAttribute("src");
+    if(current == img1URL){
+        imgElement.setAttribute("src", img2URL);
+    }
+    else{
+        imgElement.setAttribute("src",img1URL);
+    }
+}
+imgElement.onclick = changePhoto;
+   
